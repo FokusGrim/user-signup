340963bd56de56ca28fb0d4bb7eb86800dbe41f4
FokusGrim <andrewschrumpf@gmail.com>
2018-10-01 17:37:05 -0500

    dbjkbjbo

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, redirect, render_template
+import cgi
 import os
+import jinja2
 
 
 app = Flask(__name__)
-app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-
-
+app.config['DEBUG'] = True 
 
 
 @app.route("/", methods = ['GET', 'POST'])
